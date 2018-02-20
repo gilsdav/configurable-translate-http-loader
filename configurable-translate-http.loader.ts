a42980660b442a91fd37d5bd9f9590889b878aa2
David Gilson <davgilson@live.fr>
2018-02-20 13:14:10 +0100

    Update configurable-translate-http.loader.ts

--- configurable-translate-http.loader.ts
+++ configurable-translate-http.loader.ts
@@ -7,7 +7,7 @@ import { switchMap } from 'rxjs/operators/switchMap';
 /**
  * Wait for localesPathSubject before translate.
  *
- * Exemple : `ConfigurableTranslateHttpLoader.localesPathSubject('http://localhost:4200/assets/locales/')`.
+ * Exemple : `ConfigurableTranslateHttpLoader.localesPathSubject.next('http://localhost:4200/assets/locales/')`.
 */
 export class ConfigurableTranslateHttpLoader extends TranslateHttpLoader {
 
