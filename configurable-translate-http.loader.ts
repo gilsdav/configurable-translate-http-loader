716674e080ed5311d0f803739a4a27c0f8044d82
David Gilson <davgilson@live.fr>
2018-02-20 13:08:26 +0100

    Create configurable-translate-http.loader.ts

---
+++ configurable-translate-http.loader.ts
@@ -0,0 +1,23 @@
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { tap } from 'rxjs/operators/tap';
+import { switchMap } from 'rxjs/operators/switchMap';
+
+/**
+ * Wait for localesPathSubject before translate.
+ *
+ * Exemple : `localesPathSubject('http://localhost:4200/assets/locales/')`.
+*/
+export class ConfigurableTranslateHttpLoader extends TranslateHttpLoader {
+
+    public static localesPathSubject = new ReplaySubject<string>();
+
+    public getTranslation(lang: string) {
+        return ConfigurableTranslateHttpLoader.localesPathSubject.pipe(
+            tap(value => { this.prefix = value; }),
+            switchMap(() => super.getTranslation(lang))
+        );
+    }
+
+}
